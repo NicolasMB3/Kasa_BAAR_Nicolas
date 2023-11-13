e9c6575292b0f41ded7629d8b8c1ebb662470b0a
Nicolas <76515190+NicolasMB3@users.noreply.github.com>
2023-11-13 14:52:20 +0100

    add: build

--- src/Utils/Style.jsx
+++ src/Utils/Style.jsx
@@ -1,16 +1,13 @@
 import styled from 'styled-components';
 import colors from './Colors';
 
-const GlobalStyle = styled.div`
+const Style = styled.div`
 
-  margin: 0;
-  padding: 0;
   list-style: none;
   font-family: Montserrat, 'Courier New', Courier, sans-serif;
   color: ${colors.primary};
   background-color: ${colors.white};
   width: 100%;
-  object-fit: cover;
 
   main {
     display: flex;
@@ -42,4 +39,4 @@ const GlobalStyle = styled.div`
 
 `;
 
-export default GlobalStyle;
+export default Style;
