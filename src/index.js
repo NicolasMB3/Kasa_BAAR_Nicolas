94d9552cef0ed363408ff317556ed7826c88baed
Nicolas <76515190+NicolasMB3@users.noreply.github.com>
2023-11-17 18:31:15 +0100

    remove: delete app file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+
+import routes from './Routes';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
 import Style from './Utils/Style'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Style>
-    <App />
+    <RouterProvider router={createBrowserRouter(routes)}/>
   </Style>
 );
